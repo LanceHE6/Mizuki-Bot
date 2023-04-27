3799769cecfb88a2ca86710e4b76c35a06f72fdd
Silence <92719733+CM-Silence@users.noreply.github.com>
2023-04-27 17:57:10 +0800

    player: 定义变量以及编写构造函数

--- mizuki/plugins/test/player.py
+++ mizuki/plugins/test/player.py
@@ -7,13 +7,28 @@
 from pathlib import Path
 import json
 
-user_data=Path() / 'mizuki' / 'plugins' / 'test' / 'user_data.json'
+user_data = Path() / 'mizuki' / 'plugins' / 'test' / 'user_data.json'
+
+
+async def new_instance(uid: str | int):
+    with open(user_data, 'r', encoding='utf-8') as data:
+        user = json.load(data)
+        data.close()
+    player_level: int = user[uid]["level"]
+    player_name: str = user[uid]["name"]
+    player_skills: dict = user[uid]["skills"]["1"]
+    return Player(player_level, player_name)
+
 
 class Player:
-    def __init__(self,uid: str | int):
-        with open(user_data, 'r', encoding='utf-8') as data:
-            user=json.load(data)
-            data.close()
-        self.player_level: int =user[uid]["level"]
-        self.player_skills: dict=user[uid]["skills"]
+
+    def __init__(self, level: int, name: str, skills: dict):
+        self.player_name = name
+        self.max_health = 100 + 2 * level
+        self.health = self.max_health
+        self.attack = 40 + 2 * level
+        self.defence = 20 + 1 * level
+        self.critical_rate = 10 + 1 * level
+        self.critical_damage = 50 + 5 * level
+        self.player_skills = skills
         pass
