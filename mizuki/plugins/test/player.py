d3942080e85dbfe61289bf2f48cb95c4fb18c663
LanceHE6 <2765543491@qq.com>
2023-04-27 22:59:48 +0800

    add get information command

--- mizuki/plugins/test/player.py
+++ mizuki/plugins/test/player.py
@@ -10,25 +10,51 @@ import json
 user_data = Path() / 'mizuki' / 'plugins' / 'test' / 'user_data.json'
 
 
-async def new_instance(uid: str | int):
+async def new_instance(uid: str or int):
     with open(user_data, 'r', encoding='utf-8') as data:
         user = json.load(data)
         data.close()
-    player_level: int = user[uid]["level"]
-    player_name: str = user[uid]["name"]
-    player_skills: dict = user[uid]["skills"]["1"]
+    try:
+        player_level: int = user[f"{uid}"]["level"]
+        player_name: str = user[f"{uid}"]["name"]
+        #player_skills: dict = user[f"{uid}"]["skills"]["1"]
+    except KeyError:
+        player_level: int = 0
+        player_name: str = ''
+        #player_skills: dict = {}
+        user[f"{uid}"]={
+                "name": player_name,
+                "level": player_level,
+                "skills": {}
+        }
+        with open(user_data, 'w', encoding='utf-8') as data:
+            json.dump(user,data)
+            data.close()
+
     return Player(player_level, player_name)
 
 
 class Player:
 
-    def __init__(self, level: int, name: str, skills: dict):
+    def __init__(self, level: int, name: str):
         self.player_name = name
+        self.player_level = level
         self.max_health = 100 + 2 * level
         self.health = self.max_health
         self.attack = 40 + 2 * level
         self.defence = 20 + 1 * level
         self.critical_rate = 10 + 1 * level
         self.critical_damage = 50 + 5 * level
-        self.player_skills = skills
-        pass
+        #self.player_skills = skills
+
+    async def get_info(self)->dict:
+        player_info={
+            "name":self.player_name,
+            "level":self.player_level,
+            "max_health":self.max_health,
+            "attack":self.attack,
+            "defence":self.defence,
+            "crit_rate":self.critical_rate,
+            "crit_damage":self.critical_damage
+        }
+        return player_info
