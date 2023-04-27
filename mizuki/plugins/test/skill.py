131956d56b9bcdaaeb4032ebca63d4ae62bca7d3
Silence <92719733+CM-Silence@users.noreply.github.com>
2023-04-27 17:57:19 +0800

    skill: 定义变量以及编写构造函数

--- mizuki/plugins/test/skill.py
+++ mizuki/plugins/test/skill.py
@@ -6,6 +6,32 @@
 
 from .player import user_data
 
+
+async def new_instance(skill: dict):
+    return Skill(skill["type"], skill["level"], skill["is_using"])
+
+
 class Skill:
-    def __init__(self):
-        pass
+    def __init__(self, skill_type: int, level: int, is_using: bool):
+        self.name = ""
+        self.max_rate = 0
+        self.min_rate = 0
+        self.skill_type = skill_type
+        self.level = level
+        self.is_using = is_using
+        await self.init_type(skill_type, level)
+
+    async def init_type(self, skill_type: int, level: int):
+        if skill_type == 1:
+            self.name = "技能1"
+            self.min_rate = 10 + 1 * level
+            self.max_rate = 20 + 2 * level
+        elif skill_type == 2:
+            self.name = "技能2"
+            self.min_rate = 10 + 1 * level
+            self.max_rate = 20 + 2 * level
+        elif skill_type == 3:
+            self.name = "技能3"
+            self.min_rate = 10 + 1 * level
+            self.max_rate = 20 + 2 * level
+
