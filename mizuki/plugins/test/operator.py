5d96db661557ff3581415892cd86db0ebc51c658
Silence <92719733+CM-Silence@users.noreply.github.com>
2023-05-05 11:53:06 +0800

    数据库示例更新 Player类改名为Operator类

--- mizuki/plugins/test/operator.py
+++ mizuki/plugins/test/operator.py
@@ -15,26 +15,36 @@ uid:str         level:int           operators_all                               
   123               10           {2:{level:10,skills:{1,2,3}},...}             {2:{level:10,skills:{1,2,3}},...} 
   
 数据库干员表示例（只读
-type:int    name:str       base_health:int  base_atk:int  base_def:int  base_resistance:int  base_crit_rate:double  base_crit_damage:double   skills
-    10          芬                1200           200           150              10                    5.0                     50.0             [1,3]
+type:int    name:str       base_health:int  base_atk:int  base_def:int  base_resistance:real  base_crit_rate:real  base_crit_damage:real   skills
+    10          芬                1200           200           150              10                    0.05                     0.5          [1,3]
+    
+base_health_plus:int  base_atk_plus:int  base_def_plus:int  base_resistance_plus:real  base_crit_rate_plus:real  base_crit_damage_plus:real
+         20                  10                  5                     1                         0.01                            0.05 
+           
+base_speed:real  base_speed_plus:real
+      100                1
 
 数据库技能表示例（只读
-type:int    name:str       brief_description:str         base_rate1:double    base_rate2:double
-    1         强力击            造成大量物理伤害                     2.5                 3.0
+type:int    name:str       brief_description:str         base_rate1:real    base_rate2:real   base_rate1_plus:real    base_rate2_plus:real
+    1         强力击            造成大量物理伤害                     2.5                 3.0                    0.03                    0.04
+    
+base_consumption int    base_persistence int    base_consumption_plus real    base_persistence_plus real
+        25                       2                          1                             0.25
 """
+
 
 async def new_instance(uid: str or int):
     with open(user_data, 'r', encoding='utf-8') as data:
         user = json.load(data)
         data.close()
     try:
-        player_level: int = user[f"{uid}"]["level"]
-        player_name: str = user[f"{uid}"]["name"]
-        player_skills: dict = user[f"{uid}"]["skills"]
+        operator_level: int = user[f"{uid}"]["level"]
+        operator_name: str = user[f"{uid}"]["name"]
+        operator_skills: dict = user[f"{uid}"]["skills"]
     except KeyError:
-        player_level: int = 0
-        player_name: str = ''
-        player_skills: dict = {
+        operator_level: int = 0
+        operator_name: str = ''
+        operator_skills: dict = {
             "1": {
                 "type": 1,
                 "level": 1,
@@ -42,18 +52,18 @@ async def new_instance(uid: str or int):
             }
         }
         user[f"{uid}"] = {
-            "name": player_name,
-            "level": player_level,
-            "skills": player_skills
+            "name": operator_name,
+            "level": operator_level,
+            "skills": operator_skills
         }
         with open(user_data, 'w', encoding='utf-8') as data:
             json.dump(user, data)
             data.close()
 
-    return Player(player_level, player_name, player_skills)
+    return Operator(operator_level, operator_name, operator_skills)
 
 
-class Player:
+class Operator:
 
     def __init__(self, level: int, name: str, skills: dict):
         self.player_name = name
