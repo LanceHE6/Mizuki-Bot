720606f3c7a25954f24ed0642b1e9bef080346b6
Silence <92719733+CM-Silence@users.noreply.github.com>
2023-05-06 14:41:06 +0800

    完成Operator类构造函数 以及get_operators_list函数 和new_instance函数

--- mizuki/plugins/ArkRail/operator.py
+++ mizuki/plugins/ArkRail/operator.py
@@ -5,7 +5,9 @@
 # @Software:PyCharm
 
 from pathlib import Path
-import json
+from .DB import get_user_playing_ops, get_op_attribute, OPAttribute
+from skill import get_skills_list
+from skill import Skill
 
 user_data = Path() / 'mizuki' / 'plugins' / 'ArkRail' / 'user_data.json'
 
@@ -16,32 +18,56 @@ uid:str         level:int           operators_all                               
 """
 
 
-async def new_instance_list(uid: str or int):
-
-    return
-
-
 class Operator:
 
-    def __init__(self, level: int, name: str, skills: dict):
-        self.player_name = name
-        self.player_level = level
-        self.max_health = 2000 + 30 * level
-        self.health = self.max_health
-        self.attack = 200 + 4 * level
-        self.defence = 150 + 3 * level
-        self.critical_rate = 10 + int(level / 10)
-        self.critical_damage = 50 + level
-        self.player_skills = skills
+    def __init__(self, name: str, level: int, health: int, atk: int,
+                 defence: int, res: float, crit_r: float, crit_d: float, speed: float,
+                 atk_type: int, skills_list: list[Skill]):
+        self.name = name
+        self.level = level
+        self.health = health
+        self.atk = atk
+        self.defence = defence
+        self.res = res
+        self.crit_r = crit_r
+        self.crit_d = crit_d
+        self.speed = speed
+        self.atk_type = atk_type
+        self.skills_list = skills_list
 
-    async def get_info(self) -> dict:
-        player_info = {
-            "name": self.player_name,
-            "level": self.player_level,
-            "max_health": self.max_health,
-            "attack": self.attack,
-            "defence": self.defence,
-            "crit_rate": self.critical_rate,
-            "crit_damage": self.critical_damage
-        }
-        return player_info
+
+async def new_instance(oid, level, skills_level):
+    name = await get_op_attribute(oid, OPAttribute.name)
+    health = (await get_op_attribute(oid, OPAttribute.health) +
+              await get_op_attribute(oid, OPAttribute.health_plus) * level)
+    atk = (await get_op_attribute(oid, OPAttribute.atk) +
+           await get_op_attribute(oid, OPAttribute.atk_plus) * level)
+    defence = (await get_op_attribute(oid, OPAttribute.defence) +
+               await get_op_attribute(oid, OPAttribute.defence_plus) * level)
+    res = (await get_op_attribute(oid, OPAttribute.res) +
+           await get_op_attribute(oid, OPAttribute.res_plus) * level)
+    crit_r = (await get_op_attribute(oid, OPAttribute.crit_r) +
+              await get_op_attribute(oid, OPAttribute.crit_r_plus) * level)
+    crit_d = (await get_op_attribute(oid, OPAttribute.crit_d) +
+              await get_op_attribute(oid, OPAttribute.crit_d_plus) * level)
+    speed = (await get_op_attribute(oid, OPAttribute.speed) +
+             await get_op_attribute(oid, OPAttribute.speed_plus) * level)
+    atk_type = await get_op_attribute(oid, OPAttribute.atk_type)
+    sid_list = await get_op_attribute(oid, OPAttribute.skills)
+    skills_list: list[Skill] = await get_skills_list(sid_list, skills_level)
+
+    return Operator(name, level, health, atk, defence, res, crit_r, crit_d, speed, atk_type, skills_list)
+
+
+async def get_operator_list(uid: str or int) -> list[Operator]:
+    playing_ops_dict = await get_user_playing_ops(uid)
+    playing_ops_list: list[Operator] = []
+
+    for op in playing_ops_dict:
+        oid = playing_ops_dict[op]["oid"]
+        level = playing_ops_dict[op]["level"]
+        skills_level = playing_ops_dict[op]["skills_level"]  # [0,1,1]
+        op_instance = await new_instance(oid, level, skills_level)
+        playing_ops_list.append(op_instance)
+
+    return playing_ops_list
