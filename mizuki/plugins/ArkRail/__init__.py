047c8210fdf772d28ee097711a17ea5b7206df5e
Silence <92719733+CM-Silence@users.noreply.github.com>
2023-05-06 17:43:38 +0800

    干员细节查看指令功能完善

--- mizuki/plugins/ArkRail/__init__.py
+++ mizuki/plugins/ArkRail/__init__.py
@@ -7,9 +7,9 @@
 from nonebot import on_command
 from nonebot.adapters.onebot.v11 import GroupMessageEvent, MessageSegment, Message
 from nonebot.params import CommandArg, ArgPlainText
-from .DB import is_in_table, get_user_playing_ops, get_user_all_ops, get_op_attribute,get_oid_by_name ,OPAttribute
+from .DB import is_in_table, get_user_playing_ops, get_user_all_ops, get_op_attribute, get_oid_by_name, OPAttribute
 from .DB import is_op_owned
-from .operator import new_instance
+from .operator import new_instance, Operator
 
 op_info = on_command("info", aliases={"我的干员", "干员"}, block=True, priority=2)
 op_info_all = on_command("info all", aliases={"所有角色", "所有干员"}, block=True, priority=2)
@@ -62,19 +62,30 @@ async def _(event: GroupMessageEvent, args: Message = CommandArg()):
     if oid == -1:
         await op_detail.finish(MessageSegment.at(uid) + f"没有名为{name}的干员")
 
-    if is_op_owned(uid, oid):
-        pass
-        #玩家拥有该干员
+    op: Operator
+    tip: str = ""
+    if await is_op_owned(uid, oid):
+        # 玩家拥有该干员
+        tip = "干员的详细信息为："
+        user_all_ops = await get_user_all_ops(uid)
+        index = "1"
+        for i in user_all_ops:
+            if user_all_ops[i]["oid"] == oid:
+                index = i
+                break
+
+        select_op = user_all_ops[index]
+        oid = select_op["oid"]
+        level = select_op["level"]
+        skills_level = select_op["skills_level"]
+        op = await operator.new_instance(oid, level, skills_level)
     else:
+        tip = "您未拥有该干员，以下是该干员的初始状态信息"
+        op = await operator.new_instance(oid, 1, [0, 0, 0])
         pass
-        #玩家未拥有该干员
-    playing_ops = await get_user_playing_ops(uid)
-    first_op = playing_ops["1"]
-    oid = first_op["oid"]
-    level = first_op["level"]
-    skills_level = first_op["skills_level"]
-    op = await operator.new_instance(oid, level, skills_level)
-    reply_op_info = MessageSegment.at(uid) + "您的第一个出战干员的详细信息为：\n" \
+        # 玩家未拥有该干员
+
+    reply_op_info = MessageSegment.at(uid) + f"{tip}\n" \
                                              f"{op.name}\n等级：{op.level}  最大生命值：{op.max_health}\n" \
                                              f"攻击力：{op.atk}\n攻击方式：{op.atk_type_str}\n" \
                                              f"防御力：{op.defence}\n法抗：{op.res}\n" \
@@ -84,9 +95,10 @@ async def _(event: GroupMessageEvent, args: Message = CommandArg()):
     reply_skills_info = MessageSegment.at(uid) + f"{op.name}的技能数据为："
     i = 1
     for skill in op_skills_list:
-        reply_skills_info += f"\n{i}. {skill.name}  等级：{skill.level}\n" \
+        reply_skills_info += f"\n\n{i}. {skill.name}  等级：{skill.level}\n" \
                              f"技力消耗：{int(skill.consume)}\n" \
                              f"{skill.detail}"
+        i += 1
     await op_detail.send(reply_op_info)
     await op_detail.send(reply_skills_info)
     await op_detail.finish()
