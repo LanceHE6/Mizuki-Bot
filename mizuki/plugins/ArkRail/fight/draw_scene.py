d46b00d74bb5f2ac0161b73fec2a15b4c533854b
Silence <92719733+CM-Silence@users.noreply.github.com>
2023-05-28 00:03:39 +0800

    [upgrade]增加一些图片资源

---
+++ mizuki/plugins/ArkRail/fight/draw_scene.py
@@ -0,0 +1,8 @@
+# -*- coding = utf-8 -*-
+# @File:draw_scene.py
+# @Author:Silence
+# @Time:2023/5/24 17:39
+# @Software:PyCharm
+from PIL import Image, ImageDraw, ImageFont
+from pathlib import Path
+
