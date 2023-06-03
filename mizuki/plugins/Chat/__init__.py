79101d60782aefc081cf760a98b0aab35d53fee7
LanceHE6 <2765543491@qq.com>
2023-06-03 15:36:26 +0800

    修复chatgpt第三方api无法使用的问题（增加请求头）

--- mizuki/plugins/Chat/__init__.py
+++ mizuki/plugins/Chat/__init__.py
@@ -33,13 +33,18 @@ __plugin_info__ = PluginInfo(
 @chat.handle()
 async def reply(event: Event):
     spoken = str(event.get_message())
-    #data["spoken"] = str(event.get_message())
-    #api = 'https://api.ownthink.com/bot'#思知bot
+    # data["spoken"] = str(event.get_message())
+    # api = 'https://api.ownthink.com/bot'#思知bot
 
-    #result = json.loads(requests.post(url=api, data=json.dumps(data)).content)
-    #message = result['data']['info']['text']
-    #第三方api http://api.sc1.fun/?action=doc&id=3
+    # result = json.loads(requests.post(url=api, data=json.dumps(data)).content)
+    # essage = result['data']['info']['text']
+    # 第三方api http://api.sc1.fun/?action=doc&id=3
     api = f'http://api.sc1.fun/API/ChatGPT.php?msg={spoken}&type=wifi&id=1&mos=json'
-    result = json.loads(requests.get(url=api).content)["message"]
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.57 "
+    }
+    result = json.loads(requests.get(url=api, headers=headers).content)["message"]
+
     await chat.finish(MessageSegment.at(event.get_user_id()) + Message(result))
 
