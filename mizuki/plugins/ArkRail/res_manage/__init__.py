f68f981b36bb79bc1a9002197f3a76bf3b6d9321
LanceHE6 <2765543491@qq.com>
2023-06-04 16:51:58 +0800

    修改仓库api请求参数

--- mizuki/plugins/ArkRail/res_manage/__init__.py
+++ mizuki/plugins/ArkRail/res_manage/__init__.py
@@ -26,7 +26,9 @@ def check_release():
             则返回字符串信息
     """
     headers = {
-        "Authorization": "token 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"
+        # "Authorization": "token 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",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.57 "
     }
     release_api = 'https://api.github.com/repos/LanceHE6/Mizuki-Bot/releases'
 
