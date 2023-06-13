d156e3290ba48d1e85e8d484d54fd51d28d99b6b
Silence <92719733+CM-Silence@users.noreply.github.com>
2023-06-14 01:47:40 +0800

    [fix]修复一些bug [doc]完善注释 [upgrade]现在绘制战斗信息图片函数可以将信息字符串也绘制出来了

--- mizuki/plugins/ArkRail/fight/single_player_combat.py
+++ mizuki/plugins/ArkRail/fight/single_player_combat.py
@@ -14,7 +14,7 @@ from .playing_manager import PlayingManager, new_instance
 from ...Utils.PluginInfo import PluginInfo
 from ..DB import is_map_exist, get_map_attribute, MapAttribute, user_agar
 from ...Currency.utils import change_user_lmc_num, change_user_sj_num
-from .draw_image import draw_fight_image
+from .draw_image import draw_player_fight_image
 
 play = on_command("play", aliases={"作战"}, block=True, priority=1)
 playing_user: list[int] = []  # 正在进行战斗的用户
@@ -121,110 +121,241 @@ async def _(event: GroupMessageEvent, args: Message = CommandArg()):
 
     pm: PlayingManager = await new_instance(uid, mid)  # 战斗数据
     playing_user.append(uid)  # 将用户id放进战斗中的用户id列表
-    await send_status_image(pm)
 
     operate_atk = on_command("atk", aliases={"attack", "普通攻击", "普攻", "攻击"}, rule=is_doctor, block=True, priority=1)
     operate_skill = on_command("skill", aliases={"技能", "使用技能"}, rule=is_doctor, block=True, priority=1)
     operate_run = on_command("run", aliases={"逃跑", "润", "溜了"}, rule=is_doctor, block=True, priority=1)
 
+    # 先进行一次判断，如果是敌人先手则敌方先行动
     await send_message_and_is_over(await pm.is_enemy_turn(), play)
+    await send_status_image(pm)  # 绘制战斗数据图片
+
+    # 后面就是命令定义了
 
     @operate_atk.handle()
     async def _(atk_args: Message = CommandArg()):
+        """
+        干员普攻指令/atk [目标序号(int)]
+        :param atk_args: [目标序号(int)]，只有当干员的攻击类型为“不攻击”时才能为空
+        """
+        message: list[str] = []  # 消息列表
+        # 判断是否是我方回合，如果不是的话则退出当前方法
         if pm.all_list[0] not in pm.all_ops_list:
-            await operate_atk.finish("现在还不是你的回合哦！")
-        op = pm.all_list[0]  # 行动干员
-        if op.atk_type_p == 9:
-            await send_message_and_is_over(await pm.turn(op, 0), operate_atk)
+            await operate_atk.finish(f"现在是{pm.all_list[0].name}的回合哦！")
+
+        # 获取当前行动干员
+        op = pm.all_list[0]
+
+        # 根据行动干员攻击类型的不同做出不同的操作
+        if op.atk_type_p == 9:  # 9代表不攻击，不攻击的干员不需要参数
+            message = await pm.turn(op, 0)
+            await send_message_and_is_over(message, operate_atk)
+
+        # 如果输入的参数无法转换为数字
         elif not str(atk_args).isdigit():
-            await operate_atk.finish("参数错误！\n/atk <目标序号>\ntip:不普攻的干员可以不选目标")
+            await operate_atk.finish("参数错误！\n/atk <目标序号>\ntip:不普攻的干员才可以不选目标")
+
+        # 正常普攻干员的普攻逻辑在下面
         else:
             obj_num = int(str(atk_args))  # 目标序号
+
+            # 如果干员的攻击目标是敌人(如果是治疗型干员的话“攻击”目标就是我方干员了)
             if op.atk_type_p in [0, 1, 2, 3, 6, 7, 8]:
-                obj = pm.all_enemies_list[obj_num - 1]  # 目标对象
+
+                # 如果输入的参数不合理(超出敌人列表索引范围)
+                if not 0 < obj_num <= len(pm.all_enemies_list):
+                    await operate_atk.finish("目标序号错误！\n/atk <敌人序号>")
+
+                # 目标对象
+                obj = pm.all_enemies_list[obj_num - 1]
+
+                # 如果干员被嘲讽的话，就只能以嘲讽对象为攻击目标
                 if op.mocked and op.mocking_obj != obj:
                     await operate_atk.finish(
                         f"你被{pm.all_enemies_list.index(op.mocking_obj) + 1}.{op.mocking_obj.name}嘲讽了！\n只能以ta为攻击目标！")
-                if 0 < obj_num <= len(pm.all_enemies_list):
-                    await send_message_and_is_over(await pm.turn(op, 0, obj), operate_atk)
-                else:
-                    await operate_atk.finish("目标序号错误！\n/atk <敌人序号>")
-            else: 
-                obj = pm.all_ops_list[obj_num - 1]  # 目标对象
-                if 0 < obj_num <= len(pm.all_ops_list):
-                    await send_message_and_is_over(await pm.turn(op, 0, obj), operate_atk)
-                else:
+
+                # 进行普攻
+                message = await pm.turn(op, 0, obj)
+                await send_message_and_is_over(message, operate_atk)
+
+            # 如果干员的攻击目标是我方干员
+            else:
+
+                # 如果输入的参数不合理(超出我方干员列表索引范围)
+                if not 0 < obj_num <= len(pm.all_ops_list):
                     await operate_atk.finish("目标序号错误！\n/atk <友方序号>")
-        await send_status_image(pm)
-        await send_message_and_is_over(await pm.is_enemy_turn(), operate_atk)
+
+                # 目标对象
+                obj = pm.all_ops_list[obj_num - 1]
+
+                # 进行普攻
+                message = await pm.turn(op, 0, obj)
+                await send_message_and_is_over(message, operate_atk)
+
+        # 我方行动后判断是否是敌方干员回合，如果是则让敌方干员行动
+        message2 = await pm.is_enemy_turn()
+        message += message2
+        await send_message_and_is_over(message2, operate_atk)
+
+        # 再次等到我方干员回合再绘制战斗状态图
+        await send_status_image(pm, message)
+
+        # 方法结束
         await operate_atk.finish()
 
     @operate_skill.handle()
     async def _(skill_args: Message = CommandArg()):
+        """
+        干员释放技能指令/skill <技能序号(int)> [目标序号1(int)] [目标序号2(int)]
+
+        目标类型列表
+        0:自身  1:单个敌人  2:两个敌人  3:所有敌人  4:单个我方  5:两个我方  6:所有我方  7:单个敌方和单个我方
+
+        :param skill_args: <技能序号(int)> [目标序号1(int)] [目标序号2(int)]，技能序号必填，根据技能类型选填目标序号
+        """
+        message: list[str] = []  # 消息列表
+
+        # 判断是否是我方干员回合
         if pm.all_list[0] not in pm.all_ops_list:
             await operate_skill.finish("现在还不是你的回合哦！")
+
+        # 如果干员被沉默则无法使用技能
         if pm.all_list[0].silent:
             await operate_skill.finish("你被沉默了，无法使用技能！")
+
+        # 将命令参数字符串分解为参数列表(str型)
         parm_str_list: list[str] = str(skill_args).split(" ")
+
+        # 参数列表(int型)
         parm_list: list[int] = []
+
+        # 将str型参数列表转换为int型参数列表
         for n in parm_str_list:
+
+            # 判断输入的参数是否是数字
             if n.isdigit():
+                # 将str型参数转换为int型参数(因为skill列表索引从0开始，所以要-1)
                 parm_list.append(int(n) - 1)
             else:
                 await operate_run.finish("参数错误！\n/skill <技能序号> [目标序号1] [目标序号2/友方序号]")
-        skill_num = parm_list[0]  # 技能序号(原始)
+
+        skill_num = parm_list[0]  # 技能序号(int)
         op = pm.all_list[0]  # 行动干员
         skill = None  # 使用的技能
+
+        # 判断技能序号输入是否正确
         if 0 <= skill_num < len(op.skills_list):
+            # 获取干员要使用的技能
             skill = op.skills_list[skill_num]
         else:
             await operate_skill.finish("该技能不存在！")
 
+        # 判断玩家当前技力点是否足够
         if skill.consume > pm.player_skill_count:
             await operate_skill.finish("您的技力点不足以释放这个技能！")
+
+        # 判断技能是否在持续时间内
         elif skill.count > 0:
             await operate_skill.finish("该技能还在持续时间内！")
 
-        if skill.obj_type in [1, 4]:  # 单攻或扩散技能
-            if len(parm_list) >= 2 and 0 <= parm_list[1] < len(pm.all_enemies_list):
+        # 以下是技能释放逻辑
+        # 单体技能
+        if skill.obj_type in [1, 4]:
+            # 如果参数大于两个 且 ((技能对敌 且 敌人序号参数正确) 或 (技能对友 且 友方序号参数正确))
+            if len(parm_list) >= 2 and \
+                    ((skill.obj_type == 1 and 0 <= parm_list[1] < len(pm.all_enemies_list)) or
+                     (skill.obj_type == 4 and 0 <= parm_list[1] < len(pm.ops_list))):
+
+                # 获取目标
                 obj1 = pm.all_enemies_list[parm_list[1]] if skill.obj_type == 1 else pm.all_ops_list[parm_list[1]]
+
+                # 如果是对敌技能且干员被嘲讽则只能以被嘲讽者为目标
                 if skill.obj_type == 1 and op.mocked and op.mocking_obj != obj1:
                     await operate_atk.finish(
                         f"你被{pm.all_enemies_list.index(op.mocking_obj) + 1}.{op.mocking_obj.name}嘲讽了！\n只能以ta为攻击目标！")
-                await send_message_and_is_over(await pm.turn(op, skill_num + 1, obj1), operate_skill)
+
+                # 干员释放技能
+                message = await pm.turn(op, skill_num + 1, obj1)
+                await send_message_and_is_over(message, operate_skill)
+
+            # 参数错误则退出方法
             else:
-                await operate_run.finish("参数不足或敌人序号错误！\n/skill <技能序号> <目标序号>")
-        elif skill.obj_type in [2, 5]:  # 双攻技能
-            if len(parm_list) >= 3 and 0 <= parm_list[1] < len(pm.all_enemies_list) and \
-                    0 <= parm_list[2] < len(pm.all_enemies_list) and parm_list[1] != parm_list[2]:
+                await operate_run.finish("参数不足或序号错误！\n/skill <技能序号> <目标序号>")
+
+        # 双目标技能(对敌或对友)
+        elif skill.obj_type in [2, 5]:
+            # 如果参数大于3个 且 两个目标参数不相同 且 ((对敌技能 且 两个敌方目标参数正确) 或 (对友技能 且 两个友方目标参数正确))
+            if len(parm_list) >= 3 and \
+                    parm_list[1] != parm_list[2] and\
+                    ((skill.obj_type == 2 and
+                      0 <= parm_list[1] < len(pm.all_enemies_list) and
+                      0 <= parm_list[2] < len(pm.all_enemies_list)) or
+                     (skill.obj_type == 5 and
+                      0 <= parm_list[1] < len(pm.all_ops_list) and
+                      0 <= parm_list[2] < len(pm.all_ops_list))):
+
+                # 获取两个目标
                 obj1 = pm.all_enemies_list[parm_list[1]] if skill.obj_type == 2 else pm.all_ops_list[parm_list[1]]
                 obj2 = pm.all_enemies_list[parm_list[2]] if skill.obj_type == 2 else pm.all_ops_list[parm_list[2]]
+
+                # 如果为对敌技能则嘲讽对象必须为目标之一
                 if skill.obj_type == 3 and op.mocked and op.mocking_obj != obj1 and op.mocking_obj != obj2:
-                    await operate_atk.finish(
-                        f"你被{pm.all_enemies_list.index(op.mocking_obj) + 1}.{op.mocking_obj.name}嘲讽了！\nta必须为攻击目标之一！")
-                await send_message_and_is_over(await pm.turn(op, skill_num + 1, obj1, obj2), operate_skill)
+                    await operate_atk.finish(f"你被{pm.all_enemies_list.index(op.mocking_obj) + 1}.{op.mocking_obj.name}嘲讽了！\nta必须为攻击目标之一！")
+
+                # 干员释放技能
+                message = await pm.turn(op, skill_num + 1, obj1, obj2)
+                await send_message_and_is_over(message, operate_skill)
+
+            # 参数错误则退出方法
             else:
-                await operate_run.finish("参数不足或敌人序号错误！\n/skill <技能序号> <目标序号1> <目标序号2>")
-        elif skill.obj_type == 7:  # 目标为一个敌人和一个友方的技能
-            if len(parm_list) >= 3 and 0 <= parm_list[1] < len(pm.all_enemies_list) and \
+                await operate_run.finish("参数不足或序号错误！\n/skill <技能序号> <目标序号1> <目标序号2>")
+
+        # 双目标技能(对敌且对友)
+        elif skill.obj_type == 7:
+            # 如果参数大于3个 且 参数1(敌方单位)正确 且 参数2(友方单位)正确
+            if len(parm_list) >= 3 and \
+                    0 <= parm_list[1] < len(pm.all_enemies_list) and \
                     0 <= parm_list[2] < len(pm.all_ops_list):
+
+                # 获取两个目标
                 obj1 = pm.all_enemies_list[parm_list[1]]
                 obj2 = pm.all_ops_list[parm_list[2]]
+
+                # 如果干员被嘲讽则只能以被嘲讽者为目标
                 if op.mocked and op.mocking_obj != obj1:
-                    await operate_atk.finish(
-                        f"你被{pm.all_enemies_list.index(op.mocking_obj) + 1}.{op.mocking_obj.name}嘲讽了！\n只能以ta为攻击目标！")
-                await send_message_and_is_over(await pm.turn(op, skill_num + 1, obj1, obj2), operate_skill)
+                    await operate_atk.finish(f"你被{pm.all_enemies_list.index(op.mocking_obj) + 1}.{op.mocking_obj.name}嘲讽了！\n只能以ta为攻击目标！")
+
+                # 干员释放技能
+                message = await pm.turn(op, skill_num + 1, obj1, obj2)
+                await send_message_and_is_over(message, operate_skill)
+
+            # 参数错误则退出方法
             else:
                 await operate_run.finish("参数不足或敌人序号错误！\n/skill <技能序号> <目标序号> <友方序号>")
+
+        # 不需要指定目标的技能(全体类技能或目标是自己的技能)
         else:
-            await send_message_and_is_over(await pm.turn(op, skill_num + 1), operate_skill)
-        await send_status_image(pm)
-        await send_message_and_is_over(await pm.is_enemy_turn(), operate_skill)
+            # 干员释放技能
+            message = await pm.turn(op, skill_num + 1)
+            await send_message_and_is_over(message, operate_skill)
+
+        # 干员释放技能后判断是否是敌方干员回合，如果是则敌方行动
+        message2 = await pm.is_enemy_turn()
+        message += message2
+        await send_message_and_is_over(message2, operate_skill)
+
+        # 绘制战斗数据
+        await send_status_image(pm, message)
+
+        # 方法结束
         await operate_skill.finish()
 
     @operate_run.handle()
     async def _():
-        await operate_run.send(f"{event.sender.nickname}逃跑了！")
+        """
+        撤退命令/run，可直接退出作战
+        """
+        await operate_run.send(f"{event.sender.nickname}战略性撤退了！")
         await finish_playing()
 
     async def finish_playing():
@@ -252,33 +383,13 @@ async def delete_handle(obj: Type[Matcher]):
     del obj
 
 
-async def send_status_image(pm: PlayingManager):
+async def send_status_image(pm: PlayingManager, message_list=None):
     """
     发送所有参战人员状态图片的函数
 
     :param pm: PlayingManage对象，包含了这场战斗的所有数据
+    :param message_list: 消息列表
     """
-    await draw_fight_image(pm)
-    # i = 1
-    # reply1 = "我方干员："
-    # for op in pm.all_ops_list:
-    #     reply1 += f"\n{i}.{op.name} {op.health}❤"
-    #     i += 1
-    # reply1 += f"\n{pm.player_skill_count}◈"
-    #
-    # j = 1
-    # reply2 = "敌方干员："
-    # for op in pm.all_enemies_list:
-    #     reply2 += f"\n{j}.{op.name} {op.health}❤"
-    #     j += 1
-    # reply2 += f"\n{pm.enemy_skill_count}◈"
-    #
-    # k = 1
-    # reply3 = "行动顺序："
-    # for op in pm.all_list:
-    #     reply3 += f"\n{k}.{op.name} {op.speed_p}↗"
-    #     k += 1
-    #
-    # await handle.send(reply1)
-    # await handle.send(reply2)
-    # await handle.send(reply3)
+    await draw_player_fight_image(pm, message_list)  # 绘制战斗图片
+    for op in pm.all_ops_list:  # 清空伤害列表
+        op.damage_list.clear()
