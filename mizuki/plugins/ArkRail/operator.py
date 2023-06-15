99c2b9d073ed59a81fffcfd69b6341297b112deb
Silence <92719733+CM-Silence@users.noreply.github.com>
2023-06-15 22:56:05 +0800

    [upgrade]物理伤害计算机制改变 [upgrade]实现干员初始效果

--- mizuki/plugins/ArkRail/operator.py
+++ mizuki/plugins/ArkRail/operator.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from .DB import get_user_playing_ops, get_op_attribute, OPAttribute, get_map_attribute, MapAttribute
 from .skill import get_skills_list
 from .skill import Skill
-from .effect import Effect
+from .effect import Effect, new_effect_instance
 
 user_data = Path() / 'mizuki' / 'plugins' / 'ArkRail' / 'user_data.json'
 
@@ -22,7 +22,7 @@ class Operator:
 
     def __init__(self, oid: int, name: str, level: int, stars: int, profession: str, health: int, atk: int,
                  defence: int, res: float, crit_r: float, crit_d: float, speed: float,
-                 atk_type: int, skills_list: list[Skill], is_enemy: bool):
+                 atk_type: int, skills_list: list[Skill], effect_list: list[Effect], is_enemy: bool):
         """
         :param oid: 干员id
         :param name: 干员名称
@@ -38,6 +38,7 @@ class Operator:
         :param speed: 干员初始速度
         :param atk_type: 干员攻击方式
         :param skills_list: 干员的技能列表
+        :param effect_list: 效果列表(开局就拥有的效果)
         :param is_enemy: 是否为敌人
 
         后面有_p的变量表示干员战斗时该变量的实际值(xxx_p = xxx * (1 + xxx_add_f) + xxx_add_d)
@@ -137,9 +138,11 @@ class Operator:
         self.mocked: int = 0
         self.mocking_obj = None
 
-        self.effect_list: list[Effect] = []
+        self.effect_list: list[Effect] = effect_list
         self.next_operators: list = []
         self.damage_list = []
+
+        self.init_effect()
 
     async def hurt(self, sub, atk_type: int, damage: int, ignore_def: float = 0, ignore_res: float = 0) -> int:
         """
@@ -154,27 +157,35 @@ class Operator:
         """
         result = 0  # 返回的伤害或治疗量
         if not self.invincible:  # 自身处于无敌状态则不会扣血
-            if atk_type in [0, 2, 8]:  # 物理伤害
+            if atk_type in [0, 2, 8, 9]:  # 物理伤害
                 defence = self.defence_p * (1 - ignore_def)
-                result = (damage - defence) \
-                    if (damage - defence > damage * 0.05) \
-                    else (damage * 0.05)  # 5%攻击力的保底伤害
+                result = int(damage * (damage / (damage + 2 * defence)))
             elif atk_type in [1, 3]:  # 法术伤害
                 res = self.res_p * (1 - ignore_res)
                 result = damage * ((100 - res) / 100) \
-                    if (damage * ((100 - res) / 100) > damage * 0.1) \
-                    else (damage * 0.1)  # 10%攻击力的保底伤害
+                    if (damage * ((100 - res) / 100) > damage * 0.2) \
+                    else int(damage * 0.1)  # 10%攻击力的保底伤害
             elif atk_type in [6, 7]:  # 真实伤害
                 result = damage
 
         # 无敌状态可以受到治疗
         if atk_type in [4, 5]:
-            result = damage
+            result = int(damage)
 
         result = int(result)
-        if atk_type in [0, 1, 2, 3, 6, 7, 8]:
+        if atk_type in [0, 1, 2, 3, 6, 7, 8, 9]:
             self.health -= result
             self.damage_list.append(int(-1 * result))  # 将伤害值存进伤害表里(负数)
+
+            # 受到伤害后会改变的属性
+            for e in self.effect_list:
+                if e.effect_type == 11 and e.effect_level < e.max_level:
+                    e.effect_level += 1
+                elif e.effect_type == 15:
+                    e.effect_level -= 1
+                    if e.effect_level <= 0:
+                        self.effect_list.remove(e)
+
         elif atk_type in [4, 5]:
             self.health += result
             if self.health > self.max_health_p:
@@ -182,13 +193,7 @@ class Operator:
                 self.health = self.max_health_p
                 result = result - (temp - self.max_health_p)
             self.damage_list.append(int(result))  # 将治疗量存进伤害表里(正数)
-        for e in self.effect_list:
-            if e.effect_type == 11 and e.effect_level < e.max_level:
-                e.effect_level += 1
-            elif e.effect_type == 15:
-                e.effect_level -= 1
-                if e.effect_level <= 0:
-                    self.effect_list.remove(e)
+
         if sub is not None:
             for e in sub.effect_list:
                 if e.effect_type == 16 and e.effect_level < e.max_level:
@@ -210,9 +215,10 @@ class Operator:
             return False
 
         for e in self.effect_list:
+
             if e.effect_type == 12:
-                stage_2 = new_instance(int(e.effect_degree), self.level, [0, 0, 0], self.is_enemy)
-                self.__dict__.update(stage_2.__dict__)  # 更新干员属性
+                stage_2 = await new_instance(int(e.effect_degree), self.level, [0, 0, 0], self.is_enemy)
+                await self.obj_cpy(stage_2)  # 更新干员属性
                 return False
         return True
 
@@ -230,7 +236,7 @@ class Operator:
             e.persistence -= 1
 
             if e.persistence == 0:  # 持续时间结束
-                if e.effect_id == 25:  # 死战效果结束干员直接倒地
+                if e.effect_type == 25:  # 死战效果结束干员直接倒地
                     self.health = -10000
 
                 finish_effect_list.append(e)  # 将技能放入待移除列表(直接在这里移除的话该循环会出现遍历异常)
@@ -330,6 +336,76 @@ class Operator:
         self.max_speed_p = self.max_speed + self.speed_add_d
         self.max_speed_p = 10 if self.max_speed_p < 10 else self.max_speed_p
 
+    def init_effect(self):
+        """
+        更新干员各项属性的方法
+        """
+
+        for e in self.effect_list:
+            # 根据效果种类给予属性加成
+            e_t = e.effect_type
+            e_d = e.effect_degree
+            if e_t == 0:
+                self.atk_add_f += e_d
+            elif e_t == 1:
+                self.def_add_f += e_d
+            elif e_t == 2:
+                self.health_add_f += e_d
+            elif e_t == 3:
+                self.res_add_f += e_d
+            elif e_t == 4:
+                self.atk_add_d += e_d
+            elif e_t == 5:
+                self.def_add_d += e_d
+            elif e_t == 6:
+                self.health_add_d += e_d
+            elif e_t == 7:
+                self.res_add_d += e_d
+            elif e_t == 8:
+                self.crit_r_add_d += e_d
+            elif e_t == 9:
+                self.crit_d_add_d += e_d
+            elif e_t == 10:
+                self.speed_add_d += e_d
+            elif e_t == 11:
+                self.atk_add_f += (e_d * e.effect_level)
+            elif e_t == 13:
+                self.atk_type_p = int(e.effect_degree)
+            elif e_t == 15:
+                self.def_add_f += (e_d * e.effect_level)
+            elif e_t == 16:
+                self.atk_add_f += (e_d * e.effect_level)
+            elif e_t == 18:
+                self.mocked = 1
+            elif e_t == 19:
+                self.hidden = 1
+            elif e_t == 20:
+                self.deathless = 1
+            elif e_t == 21:
+                self.invincible = 1
+            elif e_t == 22:
+                self.silent = 1
+            elif e_t in [23, 24]:
+                self.immobile = 1
+
+        if self.mocked == 0:
+            self.mocking_obj = None
+
+        self.max_health_p = self.max_health * (1 + self.health_add_f) + self.health_add_d
+        self.max_health_p = 0 if self.max_health_p < 0 else self.max_health_p
+        self.atk_p = self.atk * (1 + self.atk_add_f) + self.atk_add_d
+        self.atk_p = 0 if self.atk_p < 0 else self.atk_p
+        self.defence_p = self.defence * (1 + self.def_add_f) + self.def_add_d
+        self.defence_p = 0 if self.defence_p < 0 else self.defence_p
+        self.res_p = self.res * (1 + self.res_add_f) + self.res_add_d
+        self.res_p = 0 if self.res_p < 0 else self.res_p
+        self.crit_r_p = self.crit_r + self.crit_r_add_d
+        self.crit_r_p = 0 if self.crit_r_p < 0 else self.crit_r_p
+        self.crit_d_p = self.crit_d + self.crit_d_add_d
+        self.crit_d_p = 0 if self.crit_d_p < 0 else self.crit_d_p
+        self.max_speed_p = self.max_speed + self.speed_add_d
+        self.max_speed_p = 10 if self.max_speed_p < 10 else self.max_speed_p
+
     async def append_effect_list(self, effect_list: list[Effect]):
         """
         给干员添加效果的方法(请不要直接将Effect列表直接用"+"或者"+="添加进effect_list中)
@@ -337,11 +413,7 @@ class Operator:
         :param effect_list: 要添加的效果列表
         """
         for e in effect_list:
-            for i in range(len(self.effect_list)):
-                if self.effect_list[i].effect_id == e.effect_id:  # 合并同id效果
-                    self.effect_list[i] = e
-                    return
-            self.effect_list.append(e)
+            await self.append_effect(e)
 
     async def append_effect(self, e: Effect):
         """
@@ -349,11 +421,67 @@ class Operator:
 
         :param e: 要添加的效果
         """
-        for i in range(len(self.effect_list)):
-            if self.effect_list[i].effect_id == e.effect_id:  # 合并同id效果
-                self.effect_list[i] = e
-                return
+        # 只可同时存在一个的效果
+        if e.effect_type == 18:
+            for i in range(len(self.effect_list)):
+                if self.effect_list[i].effect_type == 18:  # 嘲讽效果值同时存在一个
+                    self.effect_list[i] = e
+                    return
+
+        # 其他效果
+        else:
+            for i in range(len(self.effect_list)):
+                if self.effect_list[i].effect_id == e.effect_id:  # 同id效果只需要刷新持续时间
+                    self.effect_list[i].persistence = e.persistence
+                    return
+
         self.effect_list.append(e)
+
+    async def obj_cpy(self, des):
+        self.oid = des.oid
+        self.name = des.name
+        self.stars = des.stars
+        self.profession = des.profession
+        self.level = des.level
+        self.max_health = self.health = self.max_health_p = des.health
+        self.atk = self.atk_p = des.atk
+        self.defence = self.defence_p = des.defence
+        self.res = self.res_p = des.res
+        self.crit_r = self.crit_r_p = des.crit_r
+        self.crit_d = self.crit_d_p = des.crit_d
+        self.max_speed = self.speed = self.max_speed_p = des.speed
+        self.atk_type_p = self.atk_type = des.atk_type
+        self.atk_type_str = "-"
+        self.skills_list = des.skills_list
+        self.is_enemy = des.is_enemy
+
+        atk_type = des.atk_type
+
+        if atk_type == 0:
+            self.atk_type_str = "物理单体"
+        elif atk_type == 1:
+            self.atk_type_str = "法术单体"
+        elif atk_type == 2:
+            self.atk_type_str = "物理群体"
+        elif atk_type == 3:
+            self.atk_type_str = "法术群体"
+        elif atk_type == 4:
+            self.atk_type_str = "单体治疗"
+        elif atk_type == 5:
+            self.atk_type_str = "群体治疗"
+        elif atk_type == 6:
+            self.atk_type_str = "真实单体"
+        elif atk_type == 7:
+            self.atk_type_str = "真实群体"
+        elif atk_type == 8:
+            self.atk_type_str = "物理单体嘲讽"
+        elif atk_type == 9:
+            self.atk_type_str = "不进行普攻"
+
+        self.effect_list: list[Effect] = des.effect_list
+        self.damage_list = []
+
+        self.init_effect()
 
 
 async def new_instance(oid: int, level: int, skills_level: list[int], is_enemy: bool = False) -> Operator:
@@ -369,12 +497,12 @@ async def new_instance(oid: int, level: int, skills_level: list[int], is_enemy: 
     name = await get_op_attribute(oid, OPAttribute.name, is_enemy)
     stars = await get_op_attribute(oid, OPAttribute.stars, is_enemy)
     profession = await get_op_attribute(oid, OPAttribute.profession, is_enemy)
-    health = (await get_op_attribute(oid, OPAttribute.health, is_enemy) +
-              await get_op_attribute(oid, OPAttribute.health_plus, is_enemy) * (level - 1))
-    atk = (await get_op_attribute(oid, OPAttribute.atk, is_enemy) +
-           await get_op_attribute(oid, OPAttribute.atk_plus, is_enemy) * (level - 1))
-    defence = (await get_op_attribute(oid, OPAttribute.defence, is_enemy) +
-               await get_op_attribute(oid, OPAttribute.defence_plus, is_enemy) * (level - 1))
+    health = int(await get_op_attribute(oid, OPAttribute.health, is_enemy) +
+                 await get_op_attribute(oid, OPAttribute.health_plus, is_enemy) * (level - 1))
+    atk = int(await get_op_attribute(oid, OPAttribute.atk, is_enemy) +
+              await get_op_attribute(oid, OPAttribute.atk_plus, is_enemy) * (level - 1))
+    defence = int(await get_op_attribute(oid, OPAttribute.defence, is_enemy) +
+                  await get_op_attribute(oid, OPAttribute.defence_plus, is_enemy) * (level - 1))
     res = (await get_op_attribute(oid, OPAttribute.res, is_enemy) +
            await get_op_attribute(oid, OPAttribute.res_plus, is_enemy) * (level - 1))
     crit_r = (await get_op_attribute(oid, OPAttribute.crit_r, is_enemy) +
@@ -386,9 +514,13 @@ async def new_instance(oid: int, level: int, skills_level: list[int], is_enemy: 
     atk_type = await get_op_attribute(oid, OPAttribute.atk_type, is_enemy)
     sid_list = await get_op_attribute(oid, OPAttribute.skills, is_enemy)
     skills_list: list[Skill] = await get_skills_list(sid_list, skills_level, is_enemy)
+    effect_dict_list = await get_op_attribute(oid, OPAttribute.effect_list, is_enemy)
+    effect_list: list[Effect] = []
+    for e_dict in effect_dict_list:
+        effect_list.append(await new_effect_instance(e_dict))
 
     return Operator(oid, name, level, stars, profession, health, atk, defence, res, crit_r, crit_d, speed, atk_type,
-                    skills_list, is_enemy)
+                    skills_list, effect_list, is_enemy)
 
 
 async def get_operator_list(uid: str or int) -> list[Operator]:
