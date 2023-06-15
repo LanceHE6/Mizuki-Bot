6ccb4fe5ca971aff0ef76dfabab5155ba8ef865c
LanceHE6 <2765543491@qq.com>
2023-06-15 14:26:19 +0800

    优化

--- mizuki/plugins/Currency/sign_in.py
+++ mizuki/plugins/Currency/sign_in.py
@@ -6,15 +6,17 @@
 
 import datetime
 import random
-from ...database.utils import MDB
-from .utils import change_user_lmc_num
+import time
+from colorama import Fore
+
 from nonebot.log import logger
 from nonebot import on_command, on_keyword
 from nonebot.rule import to_me
 from nonebot.adapters.onebot.v11 import GroupMessageEvent, MessageSegment
-import time
-from colorama import Fore
+
 from ..Utils.PluginInfo import PluginInfo
+from ...database.utils import MDB
+from .utils import change_user_lmc_num
 
 """
 数据库签到表示例
@@ -23,7 +25,7 @@ uid:str         last_sign_in_time:int            continuous_sign-in:int
 """
 
 sign_in_by_command = on_command("签到", aliases={"打卡"}, block=True, priority=2)
-sign_in_by_message = on_keyword( {"签到", "打卡"}, block=True, rule=to_me(), priority=2)
+sign_in_by_message = on_keyword({"签到", "打卡"}, block=True, rule=to_me(), priority=2)
 
 __plugin_info__ = PluginInfo(
     plugin_name="Currency_sign_in",
@@ -44,12 +46,11 @@ async def time_to_strftime(stamp_time):
 async def sign_func(event: GroupMessageEvent):
     uid = int(event.get_user_id())
     uid_list = await MDB.find_tb_by_column("Currency_UserSignIn", "uid")
-    # print(list(uid_list))
     now_time = int(time.time())
 
     if uid not in uid_list:
-        sql_sequence = f"Insert Into Currency_UserSignIn(uid, last_sign_in_time, continuous_sign_in) values('{uid}',{now_time},1);"
-        if await  MDB.db_execute(sql_sequence) == 'ok':
+        sql_sequence = f"Insert Into Currency_UserSignIn(uid, last_sign_in_time, continuous_sign_in) values('{uid}',{now_time},1); "
+        if await MDB.db_execute(sql_sequence) == 'ok':
             logger.info(Fore.BLUE + "[Currency_Sign_in]新用户数据已添加")
             profit = random.randint(2, 10) * 1000  # 随机获得2-10k龙门币
             change_result = await change_user_lmc_num(uid, profit)
@@ -62,13 +63,10 @@ async def sign_func(event: GroupMessageEvent):
         # 获取用户在数据库中的信息
         sql_sequence = f"Select * from Currency_UserSignIn where uid={uid};"
         user_data = await MDB.db_query_column(sql_sequence)
-        print(user_data)
-        # print(user_data)
         # 获取当前时间和用户上次签到的时间
         last_sign_in_time = await time_to_strftime(user_data[1])
         time_now = datetime.datetime.strptime(time.strftime('%Y-%m-%d'), '%Y-%m-%d')
         time_last = datetime.datetime.strptime(last_sign_in_time, '%Y-%m-%d')
-        # print(time_now, time_last, (time_now - time_last).days)
         sign_days = user_data[2]
         if time_now <= time_last:
             return "你今天已经签到过了哦！明天再来吧！"
@@ -77,7 +75,7 @@ async def sign_func(event: GroupMessageEvent):
         else:  # 断签
             sign_days = 1
         sql_sequence = f"Update Currency_UserSignIn Set last_sign_in_time={now_time},continuous_sign_in={sign_days} where uid={uid};"
-        if await  MDB.db_execute(sql_sequence) == 'ok':
+        if await MDB.db_execute(sql_sequence) == 'ok':
             logger.info(Fore.BLUE + "[Currency_Sign_in]用户数据已更新")
         else:
             return "签到出错"
