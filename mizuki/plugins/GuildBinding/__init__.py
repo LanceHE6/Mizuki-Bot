ac295fa8014b0111e1b10134f0f73cbff9269254
LanceHE6 <2765543491@qq.com>
2023-06-26 17:49:48 +0800

    新增频道用户id与QQ号绑定插件

---
+++ mizuki/plugins/GuildBinding/__init__.py
@@ -0,0 +1,37 @@
+# -*- coding = utf-8 -*-
+# @File:__init__.py.py
+# @Author:Hycer_Lance
+# @Time:2023/6/26 16:27
+# @Software:PyCharm
+
+from nonebot import on_command
+from nonebot.params import CommandArg
+
+from ..Utils.GroupAndGuildMessageEvent import GuildMessageEvent
+from .utils import set_guild_bind
+from ..Help.PluginInfo import PluginInfo
+
+guild_bind = on_command("用户绑定", aliases={"bind", "绑定qq"}, block=True, priority=2)
+
+__plugin_info__ = PluginInfo(
+    plugin_name="GuildBing",
+    name="频道用户绑定",
+    description="频道用户绑定",
+    usage="bind<QQ号> ——频道用户绑定qq(频道指令)",
+    extra={
+        "author": "Hycer_Lance",
+        "version": "0.1.0",
+        "priority": 2,
+        "guild_adapted": True
+    }
+)
+
+
+@guild_bind.handle()
+async def _(event: GuildMessageEvent, args=CommandArg()):
+    gid = event.get_user_id()
+    uid = str(args)
+    if not uid.isdigit():
+        await guild_bind.finish("未知QQ号")
+    await set_guild_bind(uid, gid)
+    await guild_bind.finish("用户绑定成功")
---
+++ mizuki/plugins/GuildBinding/utils.py
@@ -0,0 +1,40 @@
+# -*- coding = utf-8 -*-
+# @File:utils.py
+# @Author:Hycer_Lance
+# @Time:2023/6/26 17:09
+# @Software:PyCharm
+
+from ...database.utils import MDB
+
+
+async def get_uid_by_guild_id(guid_id: str) -> int:
+    """
+    通过频道中的id获取用户QQ号
+    :param guid_id:
+    :return: uid
+    """
+    sql = f'Select uid From Guild_QQ_Binding Where guild_id={guid_id};'
+    result = await MDB.db_query_single(sql)
+    if not result:
+        return 0
+    else:
+        return int(result[0])
+
+
+async def set_guild_bind(uid: int or str, gid: str):
+    """
+    设置用户频道id与qq号绑定
+    :param uid: uid
+    :param gid: guild_id
+    :return: None
+    """
+    uid = int(uid)
+    sql = f"Select count(*) From Guild_QQ_Binding Where guild_id='{gid}';"
+    exists = await MDB.db_query_single(sql)
+    exists = int(exists[0])
+    if exists:
+        change_sql = f"Update Guild_QQ_Binding Set uid={uid} Where guild_id={gid};"
+        await MDB.db_execute(change_sql)
+    else:
+        add_sql = f'Insert Into Guild_QQ_Binding Values("{gid}", {uid});'
+        await MDB.db_execute(add_sql)
