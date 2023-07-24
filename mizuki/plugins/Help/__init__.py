577ddddcefd84c023fdd232eef5c2468d566b988
Hycer_Lance <92719376+LanceHE6@users.noreply.github.com>
2023-07-24 15:01:09 +0800

    Update __init__.py

--- mizuki/plugins/Help/__init__.py
+++ mizuki/plugins/Help/__init__.py
@@ -35,5 +35,4 @@ async def _(event: GroupAndGuildMessageEvent):
         await help_comm.finish(
             GroupAndGuildMessageSegment.at(event) + GroupAndGuildMessageSegment.image(event, img_path))
     img_path = await draw_help_img()
-  print("test")
     await help_comm.finish(GroupAndGuildMessageSegment.at(event) + GroupAndGuildMessageSegment.image(event, img_path))
